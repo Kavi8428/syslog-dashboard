869d2c5d9b64720e8550717b6d3a1677ffc7f871
Kavi8428 <lakshanudara987@gmail.com>
2025-04-29 12:06:28 +0530

    First Commit

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,26 +1,11 @@
-import { NextResponse } from 'next/server';
-import mysql from 'mysql2/promise';
+// app/api/users/route.js
+import pool from '../../../lib/db';
 
 export async function GET() {
-    try {
-        // Create a connection to MySQL
-        const connection = await mysql.createConnection({
-            host: process.env.DATABASE_HOST,
-            user: process.env.DATABASE_USER,
-            password: process.env.DATABASE_PASSWORD,
-            database: process.env.DATABASE_NAME,
-        });
-
-        // Query the users table
-        const [rows] = await connection.execute('SELECT * FROM collectors');
-
-        // Close the connection
-        await connection.end();
-
-        // Return the data as JSON
-        return NextResponse.json(rows);
-    } catch (error) {
-        console.error('Error connecting to MySQL:', error);
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-    }
+  try {
+    const [rows] = await pool.execute('SELECT * FROM collectors');
+    return new Response(JSON.stringify(rows), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+  }
 }
